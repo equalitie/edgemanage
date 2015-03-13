1b92bde397bcfd9888315c2cca4156c3f227b59b
Hugh Nowlan <nosmo@nosmo.me>
2015-03-13 14:49:32 +0000

    Slice window functionality for selecting a particular number of recent fetches

--- src/edgemanage/const.py
+++ src/edgemanage/const.py
@@ -7,3 +7,10 @@ FETCH_RETRY=3
 
 # Number of objects to store in fetch histories
 FETCH_HISTORY=2000
+
+# Number of seconds that define a window within which to search for
+# fetch entries. Used in a case where we want to check the last $N
+# seconds worth of values fetched for a paritcular edge to give us a
+# more accurate average than simply averaging the last FETCH_HISTORY
+# entries.
+DECISION_SLICE_WINDOW=300
--- src/edgemanage/decisionmaker.py
+++ src/edgemanage/decisionmaker.py
@@ -1,7 +1,16 @@
+from const import DECISION_SLICE_WINDOW
+
 import logging
+import time
 
 JUDGEMENT = {
-    "under_goodenough": None
+    # Last fetch is under threshold
+    "under_threshold": None,
+    # Last DECISION_SLICE_WINDOW fetches averaged under the threshold
+    "slice_under_threshold": None,
+    # The average of all FETCH_HISTORY fetches were under the
+    # threshold
+    "average_under_thresold": None,
 }
 
 class DecisionMaker(object):
@@ -15,13 +24,18 @@ class DecisionMaker(object):
         self.current_judgement[stat_store.edgename] = JUDGEMENT.copy()
 
     def check_threshold(self, good_enough):
+        print self.stat_stores.keys()
         for edgename, stat_store in self.stat_stores.iteritems():
+            time_slice = stat_store[time.time() - DECISION_SLICE_WINDOW:time.time()]
+            time_slice_avg = sum(time_slice)/len(time_slice)
             if stat_store.last_value() < good_enough:
-                logging.info("Last fetch for %s is under the threshold (%f < %f)", edgename,
+                logging.info("PASS: Last fetch for %s is under the threshold (%f < %f)", edgename,
                              stat_store.last_value(), good_enough)
+            elif time_slice_avg < good_enough:
+                logging.info("UNSURE: Last fetch for %s is NOT under the threshold but the average of the last %d items is (%f < %f)", edgename, len(time_slice), time_slice_avg, good_enough)
             # TODO check a slice of dates for a given period here - total average is not good enough
             elif stat_store.current_average() < good_enough:
-                logging.info("Last fetch for %s is NOT under the threshold but under the average (%f < %f)",
+                logging.info("UNSURE: Last fetch for %s is NOT under the threshold but under the average (%f < %f)",
                              edgename, stat_store.current_average(), good_enough)
             else:
-                logging.info("No check for %s has passed", edgename)
+                logging.info("FAIL: No check for %s has passed - last fetch time was %f", edgename, stat_store.last_value())
