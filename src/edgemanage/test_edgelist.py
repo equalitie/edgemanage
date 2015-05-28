e2b65212bc609c291daa8612051da16c58aedfa8
Hugh Nowlan <nosmo@nosmo.me>
2015-05-28 13:44:09 +0100

    no more camelCase, better tests, fix output data

--- src/edgemanage/test_edgelist.py
+++ src/edgemanage/test_edgelist.py
@@ -1,29 +1,58 @@
 #!/usr/bin/env python
 
 import unittest
-import edgelist
+from edgelist import EdgeList
 
 TEST_DOMAIN = "fakesite.deflect.ca"
 
 class EdgeListTest(unittest.TestCase):
 
-    def testRejectBadNS(self):
-        a = edgelist.EdgeList(TEST_DOMAIN)
+    def test_reject_bad_NS(self):
+        a = EdgeList()
         self.assertRaises(Exception, a.generate_zone, ("equalit.ie", "test_data", {
             "ns_records": ["adns1.easydns.com",
                            "adns2.easydns.com"]
         }))
 
-    def testSetEdgeLive(self):
-        a = edgelist.EdgeList(TEST_DOMAIN)
+    def test_set_edge_live(self):
+        a = EdgeList()
         a.add_edge("test1")
-        a.make_active("test1")
+        a.set_edge_live("test1")
         self.assertTrue(a.edges["test1"]["live"])
 
-    def testGenerateZonefile(self):
-        a = edgelist.EdgeList("deflect.ca")
-        a.add_edge("chicago7")
-        a.make_active("chicago7")
+    def test_len(self):
+        a = EdgeList()
+        a.add_edge("test1")
+        a.add_edge("test2")
+        a.add_edge("test3")
+        self.assertEqual(len(a), 3)
+
+    def test_live_count(self):
+        a = EdgeList()
+        a.add_edge("test1", live=True)
+        a.add_edge("test2")
+        a.add_edge("test3")
+        a.set_edge_live("test2")
+        self.assertEqual(a.get_live_count(), 2)
+
+    def test_state_operations(self):
+        a = EdgeList()
+        a.add_edge("test1")
+        a.add_edge("test2", state="pass")
+        a.add_edge("test3", state="pass")
+        a.add_edge("test4", state="fail")
+        # invalid state - edgelist don't care.
+        a.add_edge("test5", state="satan")
+        a.add_edge("test6", state="pass")
+
+        self.assertTrue(a.set_live_by_state("pass", 2))
+        self.assertEqual(a.get_live_count(), 2)
+        self.assertEqual(len(a.get_edges("pass")), 3)
+
+    def test_generate_zonefile(self):
+        a = EdgeList()
+        a.add_edge("example.com")
+        a.set_edge_live("example.com")
         new_zone = a.generate_zone("test.com", "test_data", {"ns_records": ["adns1.easydns.com."],
                                                              "soa_mailbox": "test.derp.com",
                                                              "soa_nameserver": "derpderpderp.com"
