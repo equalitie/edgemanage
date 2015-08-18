a3a2ad9522d595b04a45ff3dddf5049a0a15e6c6
Hugh Nowlan <nosmo@nosmo.me>
2015-08-18 13:32:36 +0100

    Include dependencies, version bump

--- src/setup.py
+++ src/setup.py
@@ -3,15 +3,21 @@ from setuptools import setup
 
 setup(
     name = "edgemanage",
-    version = "2.0.0",
+    version = "2.0.1",
     author = "Hugh Nowlan",
     author_email = "nosmo@nosmo.me",
-    description = "DNS record scraper",
+    description = "HTTP availability management tool",
     license = "Hacktivismo Enhanced-Source Software License Agreement",
     keywords = "edgemanage deflect DNS",
     url = "http://github.com/equalitie/edgemanage",
     packages=['edgemanage'],
     package_data={'edgemanage': ['templates/*.j2']},
+    install_requires=[
+        "Jinja2",
+        "setproctitle",
+        "pyyaml",
+        "futures"
+    ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
