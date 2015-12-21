6bd903339d608842886a5affdbd4984fcec00fb5
Hugh Nowlan <nosmo@nosmo.me>
2015-12-21 14:27:43 +0000

    Log canary insertion more clearly

--- src/edgemanage/edgelist.py
+++ src/edgemanage/edgelist.py
@@ -114,7 +114,10 @@ class EdgeList(object):
 
         if canary_edge:
             # Remove a selected edge and insert the canary edge
-            del(edge_list_to_write[random.randrange(len(edge_list_to_write))])
+            removed_edge = random.randrange(len(edge_list_to_write))
+            logging.info("Domain %s uses a canary edge of %s, removing %s",
+                         domain, canary_edge, edge_list_to_write[removed_edge])
+            del(edge_list_to_write[removed_edge])
             edge_list_to_write.append(canary_edge)
 
         #TODO cache looked up IP addresses, don't do this every time
