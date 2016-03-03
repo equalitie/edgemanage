2867292fc7b2078602d56af1bda935f92d94eb79
Richard King <richard@equalit.ie>
2016-03-03 19:41:24 +0100

    Make @nosmo's disgusting hack even worse to fix tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -21,10 +21,14 @@ class UtilTest(unittest.TestCase):
 
             # ayyyy lmao. Gotta have another process to get an flock
             # refused. I am a bad person and you should hate me.
+            #
+            # This is even more disgusting now tests are in a separate directory!
             test_lock_fail = subprocess.Popen(
-                ["/usr/bin/python", "-c", ("from .context import edgemanage; import sys; "
+                ["/usr/bin/python", "-c", ("import os; import sys; "
+                "sys.path.insert(0, os.path.abspath('..')); "
+                "import edgemanage; "
                 "ret = edgemanage.util.acquire_lock(open('%s', 'w')); "
-                 "sys.exit(0 if not ret else 1)") % temp_file])
+                "sys.exit(0 if not ret else 1)") % temp_file])
             returncode = test_lock_fail.wait()
             self.assertEqual(returncode, 0,
                              msg="Could lock already locked temporary file")
