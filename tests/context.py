357cc23f5a3edce63714a5dbba2adf62e77e7da5
Richard King <richard@equalit.ie>
2016-03-07 16:33:51 +0100

    Document test helper-modules

--- tests/context.py
+++ tests/context.py
@@ -1,5 +1,13 @@
+# Importing this module gives individual tests an import context that works
+# regardless of installation method.  See
+# http://docs.python-guide.org/en/latest/writing/structure/
+
+# Usage in a test module:
+#from .context import edgemanage
+
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 
 import edgemanage
--- tests/module_locator.py
+++ tests/module_locator.py
@@ -1,11 +1,25 @@
+# This module is useful for discovering programatically the module in which it
+# resides. Place it in the same directory as code that needs to discover its
+# location. In edgemanage it is used to help tests find their sample data-files
+# regardless of the present working directory.
+#
+# http://stackoverflow.com/questions/2632199/how-do-i-get-the-path-of-the-current-executed-file-in-python
+#
+# Usage:
+#import module_locator
+#my_path = module_locator.module_path()
+
+
 import os
 import sys
+
 
 def we_are_frozen():
     # All of the modules are built-in to the interpreter, e.g., by py2exe
     return hasattr(sys, "frozen")
 
 def module_path():
+    # Return the absolute filesystem path to this module
     encoding = sys.getfilesystemencoding()
     if we_are_frozen():
         return os.path.dirname(unicode(sys.executable, encoding))
