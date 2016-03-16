3b385d1ef0213a390096ba4ef4f7c8eb6e2114d1
Richard King <richard@richardskingdom.net>
2016-03-16 18:49:20 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "edgemanage",
-    version = "2.0.6",
+    version = "2.0.7",
     author = "Hugh Nowlan",
     author_email = "nosmo@nosmo.me",
     description = "HTTP availability management tool",
