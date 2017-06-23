equalitie/edgemanage

commit ef2c941bb21a93266426b7dadacf123c9501a4f0

Treat all edges under the TIMEOUT as passing
The `good_enough` value was being used to mark an edge as "pass". All
edges with a response time greater than `good_enough` were being marked
as failing.

The correct behaviour is to mark all edges with response time less than
the `FETCH_TIMEOUT` as passing. Edges which are also less than
`good_enough` are now marked as `pass_threshold`. This will prevent
currently live edges from being rotated out when they are still fast
enough.

Redmine issue #7396.

edgemanage/decisionmaker.py
@@ -20,6 +20,9 @@ class DecisionMaker(object):
     def get_judgement(self, edgename):
         return self.current_judgement[edgename]
 
+    def edge_is_passing(self, edgename):
+        return self.get_judgement(edgename) != "fail"
+
     def check_threshold(self, good_enough):
 
         ''' Check fetch response times for being under the given
@@ -54,32 +57,33 @@ class DecisionMaker(object):
                               edgename, edge_state.last_value(), edge_state.current_average())
 
             if edge_state.last_value() < good_enough:
-                logging.info("PASS: Last fetch for %s is under the threshold (%f < %f)", edgename,
-                             edge_state.last_value(), good_enough)
-                self.current_judgement[edgename] = "pass"
-                results_dict["pass"] += 1
-            elif edge_state.last_value() == const.FETCH_TIMEOUT:
-                results_dict["fail"] += 1
-                self.current_judgement[edgename] = "fail"
-                logging.info(("FAIL: Fetch time for %s is equal to the FETCH_TIMEOUT of %d. "
-                              "Automatic fail"),
-                             edgename, const.FETCH_TIMEOUT)
+                self.current_judgement[edgename] = "pass_threshold"
+                results_dict["pass_threshold"] += 1
+                logging.info("PASS: Last fetch for %s is under the good_enough threshold "
+                             "(%f < %f)", edgename, edge_state.last_value(), good_enough)
             elif time_slice and time_slice_avg < good_enough:
                 self.current_judgement[edgename] = "pass_window"
                 results_dict["pass_window"] += 1
-                logging.info("UNSURE: Last fetch for %s is NOT under the threshold but the "
-                             "average of the last %d items is (%f < %f)",
+                logging.info("UNSURE: Last fetch for %s is NOT under the good_enough threshold "
+                             "but the average of the last %d items is (%f < %f)",
                              edgename, len(time_slice), time_slice_avg, good_enough)
             elif edge_state.current_average() < good_enough:
-                results_dict["pass_average"] += 1
                 self.current_judgement[edgename] = "pass_average"
-                logging.info("UNSURE: Last fetch for %s is NOT under the threshold but under "
-                             "the average (%f < %f)",
+                results_dict["pass_average"] += 1
+                logging.info("UNSURE: Last fetch for %s is NOT under the good_enough threshold "
+                             "but under the average (%f < %f)",
                              edgename, edge_state.current_average(), good_enough)
+            elif edge_state.last_value() < const.FETCH_TIMEOUT:
+                self.current_judgement[edgename] = "pass"
+                results_dict["pass"] += 1
+                logging.info("PASS: Last fetch for %s is not under the good_enough threshold "
+                             "but is passing (%f < %f)", edgename,
+                             edge_state.last_value(), const.FETCH_TIMEOUT)
             else:
-                results_dict["fail"] += 1
                 self.current_judgement[edgename] = "fail"
-                logging.info("FAIL: No check for %s has passed - last fetch time was %f",
-                             edgename, edge_state.last_value())
+                results_dict["fail"] += 1
+                logging.info(("FAIL: Fetch time for %s is equal to the FETCH_TIMEOUT of %d. "
+                              "Automatic fail"),
+                             edgename, const.FETCH_TIMEOUT)
 
         return results_dict

edgemanage/const.py
@@ -39,4 +39,5 @@ VALID_MODES = ["available", "force", "blindforce", "unavailable"]
 
 # Valid states that an edge will be put into after a result of a
 # decision being passed upon tests.
-VALID_HEALTHS = ["pass", "fail", "pass_window", "pass_average"]
+VALID_HEALTHS = ["pass_threshold", "pass_window", "pass_average", "pass",
+                 "fail"]

edgemanage/edgemanage.py
@@ -214,9 +214,12 @@ class EdgeManage(object):
         return verification_failues
 
     def check_last_live(self):
+        """
+        A list of edges that were in use last time that are still
+        healthy now.
 
-        # A list of edges that were in use last time that are still
-        # healthy now.
+        Healthy means the fetch time is less than the good_enough threshold
+        """
         still_healthy = []
 
         if self.state_obj.last_live:
@@ -235,7 +238,8 @@ class EdgeManage(object):
             except KeyError:
                 oldlive_health = None
 
-            if oldlive_edge in self.decision.current_judgement and oldlive_health == "pass":
+            if (oldlive_edge in self.decision.current_judgement and
+                    oldlive_health == "pass_threshold"):
                 still_healthy.append(oldlive_edge)
             elif oldlive_edge not in self.decision.current_judgement:
                 logging.warning(("Discarding previously live edge %s "
@@ -243,7 +247,8 @@ class EdgeManage(object):
                                 oldlive_edge)
             else:
                 logging.debug(
-                    "Discarding previously live edge %s because it is in state %s",
+                    "Discarding previously live edge %s because it is not "
+                    "in state 'pass_threshold', current state: %s",
                     oldlive_edge,
                     self.decision.get_judgement(oldlive_edge))
 
@@ -278,12 +283,12 @@ class EdgeManage(object):
             logging.debug("Stats of canary threshold check are %s", str(canary_stats))
 
         # Get the list of edges that were 'in' (live) the last time and are
-        # still healthy
+        # still healthy (under the good_enough threshold)
         still_healthy_from_last_run = self.check_last_live()
 
         for edgename, edge_state in self.edge_states.iteritems():
             if edgename not in self.canary_data.values() and edge_state.mode == "force":
-                if self.decision.get_judgement(edgename) == "pass":
+                if self.decision.edge_is_passing(edgename):
                     logging.debug(
                         "Making host %s live because it is in mode force and it is in state pass",
                         edgename)
@@ -335,9 +340,8 @@ class EdgeManage(object):
             logging.debug("List of previously passing edges is currently %s",
                           self.edgelist_obj.get_live_edges())
 
-            # Attempt to meet demand, first with passing, then with
-            # window, then with average passing
-            for desired_state in ["pass", "pass_window", "pass_average"]:
+            # Attempt to meet demand starting with the most responsive edge states
+            for desired_state in ["pass_threshold", "pass_window", "pass_average", "pass"]:
                 filled_by_current_state = self.edgelist_obj.set_live_by_state(desired_state,
                                                                               required_edge_count)
                 if filled_by_current_state:

tests/test_decisionmaker.py
@@ -14,12 +14,17 @@ class DecisionMakerTest(EdgeStateTemplate):
 
     def _get_failing_edge_state(self):
         es = self._make_store()
-        es.add_value(GOOD_ENOUGH * 2)
+        es.add_value(edgemanage.const.FETCH_TIMEOUT)
+        return es
+
+    def _get_good_enough_edge_state(self):
+        es = self._make_store()
+        es.add_value(GOOD_ENOUGH/10)
         return es
 
     def _get_passing_edge_state(self):
         es = self._make_store()
-        es.add_value(GOOD_ENOUGH/10)
+        es.add_value(GOOD_ENOUGH*2)
         return es
 
     def _get_error_edge_state(self):
@@ -31,16 +36,28 @@ class DecisionMakerTest(EdgeStateTemplate):
         es = self._get_error_edge_state()
         dm = edgemanage.decisionmaker.DecisionMaker()
         dm.add_edge_state(es)
-        self.assertEqual(dm.check_threshold(GOOD_ENOUGH), {"fail": 1,
+        self.assertEqual(dm.check_threshold(GOOD_ENOUGH), {'fail': 1,
+                                                           'pass_threshold': 0,
                                                            'pass_window': 0,
                                                            'pass_average': 0,
                                                            'pass': 0})
 
-    def test_good_state(self):
+    def test_good_enough_state(self):
+        es = self._get_good_enough_edge_state()
+        dm = edgemanage.decisionmaker.DecisionMaker()
+        dm.add_edge_state(es)
+        self.assertEqual(dm.check_threshold(GOOD_ENOUGH), {'fail': 0,
+                                                           'pass_threshold': 1,
+                                                           'pass_window': 0,
+                                                           'pass_average': 0,
+                                                           'pass': 0})
+
+    def test_passing_state(self):
         es = self._get_passing_edge_state()
         dm = edgemanage.decisionmaker.DecisionMaker()
         dm.add_edge_state(es)
-        self.assertEqual(dm.check_threshold(GOOD_ENOUGH), {"fail": 0,
+        self.assertEqual(dm.check_threshold(GOOD_ENOUGH), {'fail': 0,
+                                                           'pass_threshold': 0,
                                                            'pass_window': 0,
                                                            'pass_average': 0,
                                                            'pass': 1})
@@ -49,7 +66,8 @@ class DecisionMakerTest(EdgeStateTemplate):
         es = self._get_failing_edge_state()
         dm = edgemanage.decisionmaker.DecisionMaker()
         dm.add_edge_state(es)
-        self.assertEqual(dm.check_threshold(GOOD_ENOUGH), {"fail": 1,
+        self.assertEqual(dm.check_threshold(GOOD_ENOUGH), {'fail': 1,
+                                                           'pass_threshold': 0,
                                                            'pass_window': 0,
                                                            'pass_average': 0,
                                                            'pass': 0})

tests/test_edge_manage_integration.py
@@ -155,7 +155,8 @@ class EdgeManageIntegration(unittest.TestCase):
 
         health_data = self.load_all_health_files()
         self.assertEqual(len(health_data), 40)
-        self.assertTrue(all([edge['health'] == "pass" for edge in health_data.values()]))
+        self.assertTrue(all([edge['health'] == "pass_threshold"
+                             for edge in health_data.values()]))
         self.assertLess(self.running_time, 1)
 
     def test20Edges20CanariesAll3Seconds(self):

commit 01840101371208eae4e4bcff85e4e22e45b0f4d9

Immediately mark edge as failed when it exceeds TIMEOUT
A bug was introduced in decisionmaker.py in ef2c941. The average response
time was being used before checking if the latest fetch exceeded the
TIMEOUT.

This would mark an offline but previously working edge as `pass_average`
and would never mark it as failed as the average would approach but never
reach the TIMEOUT value.

The fix is to check the TIMEOUT value early before checking the fetch
average.

Fixes bug introduced in ef2c941.

edgemanage/decisionmaker.py
@@ -64,6 +64,15 @@ class DecisionMaker(object):
                 results_dict["pass_threshold"] += 1
                 logging.info("PASS: Last fetch for %s is under the good_enough threshold "
                              "(%f < %f)", edgename, edge_state.last_value(), good_enough)
+            elif edge_state.last_value() == const.FETCH_TIMEOUT:
+                # FETCH_TIMEOUT must be checked before the average measurements. An edge
+                # whose most recent fetch has failed should be marked as fail even if
+                # the average value is still passing.
+                self.current_judgement[edgename] = "fail"
+                results_dict["fail"] += 1
+                logging.info(("FAIL: Fetch time for %s is equal to the FETCH_TIMEOUT of %d. "
+                              "Automatic fail"),
+                             edgename, const.FETCH_TIMEOUT)
             elif time_slice and time_slice_avg < good_enough:
                 self.current_judgement[edgename] = "pass_window"
                 results_dict["pass_window"] += 1
@@ -76,17 +85,11 @@ class DecisionMaker(object):
                 logging.info("UNSURE: Last fetch for %s is NOT under the good_enough threshold "
                              "but under the average (%f < %f)",
                              edgename, edge_state.current_average(), good_enough)
-            elif edge_state.last_value() < const.FETCH_TIMEOUT:
+            else:
                 self.current_judgement[edgename] = "pass"
                 results_dict["pass"] += 1
                 logging.info("PASS: Last fetch for %s is not under the good_enough threshold "
                              "but is passing (%f < %f)", edgename,
                              edge_state.last_value(), const.FETCH_TIMEOUT)
-            else:
-                self.current_judgement[edgename] = "fail"
-                results_dict["fail"] += 1
-                logging.info(("FAIL: Fetch time for %s is equal to the FETCH_TIMEOUT of %d. "
-                              "Automatic fail"),
-                             edgename, const.FETCH_TIMEOUT)
 
         return results_dict