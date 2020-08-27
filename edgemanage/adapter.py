d501f1406e3b31db8edad4cd6f5c820ec2f61dac
Jeremy Yen <jeremy5189@gmail.com>
2020-08-27 16:38:22 +0800

    Add methods for edge_conf

--- edgemanage/adapter.py
+++ edgemanage/adapter.py
@@ -2,6 +2,10 @@
 Non-commandline adapter, work with Django
 """
 
+from edgemanage import util
+
+import logging
+import logging.handlers
 import yaml
 import os
 
@@ -24,3 +28,37 @@ class EdgemanageAdapter(object):
 
     def get_config(self, config_str):
         return self.config[config_str] if config_str in self.config else None
+
+
+    def edge_data_exist(self, edgename):
+        return os.path.exists(os.path.join(self.config["healthdata_store"],
+                                           "%s.edgestore" % edgename))
+
+
+    def log_edge_conf(self, edgename, mode, comment):
+        handler = logging.handlers.SysLogHandler(
+            facility=logging.handlers.SysLogHandler.LOG_DAEMON)
+        logger = logging.getLogger('edge_conf')
+        logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
+        logger.info("Edge %s changed mode to %s with comment %s",
+                    edgename, mode, comment)
+
+
+    def lock_edge_conf(self):
+        """
+        Create a lock file for edge_conf
+        """
+        self.lock_f = open(self.config["lockfile"], "w")
+
+        if not util.acquire_lock(self.lock_f):
+            return False, "Couldn't acquire edge_conf lockfile"
+
+        return True
+
+
+    def unlock_edge_conf(self):
+        """
+        Close the lock file
+        """
+        self.lock_f.close()
