a46ed1a1768cb09f44180f35dc3facd6b2f8beb1
Jeremy Yen <jeremy5189@gmail.com>
2021-05-28 23:54:36 +0800

    Bump version to 2.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "edgemanage",
-    version = "2.1.2",
+    version = "2.2.0",
     author = "Donncha O Cearbhaill",
     author_email = "donncha@equalit.ie",
     description = "HTTP availability management tool",
